b00568db116096291f9d675c28f4a204fa5041b5
Srihari Vishnu <srihari.vishnu@gmail.com>
2021-08-04 03:17:05 -0400

    feat: add terminal to UI

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
